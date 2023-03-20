5cb9dc2a2c6fe4305d598e8928e14c35e7dffae0
jjgod66 <125471355+jjgod66@users.noreply.github.com>
2023-03-20 17:27:17 +0900

    20230320

---
+++ 20230320/005 mySelect.js
@@ -0,0 +1,11 @@
+const select = document.querySelector("#mySelect");
+const p = document.querySelector("#selectedValue");
+
+const callback = function (e) {
+    // console.log(e);
+    // console.log(e.target.outerText);
+    // console.log(e.srcElement.value);
+    p.innerHTML = e.target.value + " is selected";    
+};
+select.addEventListener("change", callback);
+
