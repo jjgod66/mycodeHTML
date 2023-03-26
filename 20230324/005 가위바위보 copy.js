b3700bb6246820018198d003c6265710f64f8211
jjgod66 <125471355+jjgod66@users.noreply.github.com>
2023-03-26 23:38:04 +0900

    20230326home

--- 20230324/005 가위바위보 copy.js
+++ 20230324/005 가위바위보 copy.js
@@ -1,20 +1,41 @@
 const input = prompt("가위,바위,보 중 하나를 쓰세요")
-let computer = parseInt(Math.random()*3+1);
-//1가위 2바위 3보
-if (computer===1) {
-    computer="가위"
-} else if (computer===2) {
-    computer="바위"
-} else if (computer===3) {
-    computer="보"
+let myChoice = 0;
+if (input==="가위") {
+    myChoice = 1;
+} else if (input==="바위") {
+    myChoice = 2;
+} else {
+    myChoice = 3;
 }
+
+let random = parseInt(Math.random()*3+1);
+console.log(random);
+let computerChoice = "";
+//1가위 2바위 3보
+
+if (random===1) {
+    computerChoice = "가위"
+} else if (random===2) {
+    computerChoice = "바위"
+} else {
+    computerChoice = "보"
+}
+
 const myChoiceis = document.querySelector("#myChoiceis");
 const computersChoiceis = document.querySelector("#computersChoiceis");
 const answer = document.querySelector("#answer");
 
 myChoiceis.innerHTML = "나의 선택은 " + input;
-computersChoiceis.innerHTML = "컴퓨터의 선택은 " + computer;
+computersChoiceis.innerHTML = "컴퓨터의 선택은 " + computerChoice;
 
+if ((myChoice - random) == -2 || (myChoice - random) == 1 ) {
+    console.log(typeof (myChoice-random));
+    answer.innerHTML = "이겼습니다"
+} else if (myChoice === random) {
+    answer.innerHTML = "비겼습니다"
+} else {
+    answer.innerHTML = "졌습니다"
+};
 
 
 
